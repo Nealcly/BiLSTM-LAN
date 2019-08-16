6bd26b369d5186ea95ec1353a6060a63719e02dd
Nealcly <cuileyang@westlake.edu.cn>
2019-08-16 17:27:50 +0800

    Update attention.py

--- attention.py
+++ attention.py
@@ -25,13 +25,19 @@ class multihead_attention(nn.Module):
             num_heads: An int. Number of heads.
         '''
         super(multihead_attention, self).__init__()
+        self.gpu = gpu
         self.num_units = num_units
         self.num_heads = num_heads
         self.dropout_rate = dropout_rate
         self.causality = causality
-        self.Q_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units).cuda(), nn.ReLU().cuda())
-        self.K_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units).cuda(), nn.ReLU().cuda())
-        self.V_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units).cuda(), nn.ReLU().cuda())
+        self.Q_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units), nn.ReLU())
+        self.K_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units), nn.ReLU())
+        self.V_proj = nn.Sequential(nn.Linear(self.num_units, self.num_units), nn.ReLU())
+        if self.gpu:
+            self.Q_proj = self.Q_proj.cuda()
+            self.K_proj = self.K_proj.cuda()
+            self.V_proj = self.V_proj.cuda()
+
 
         self.output_dropout = nn.Dropout(p=self.dropout_rate)
 
@@ -39,9 +45,9 @@ class multihead_attention(nn.Module):
         # keys, values: same shape of [N, T_k, C_k]
         # queries: A 3d Variable with shape of [N, T_q, C_q]
         # Linear projections
-        Q = self.Q_proj(queries.cuda()).cuda()  # (N, T_q, C)
-        K = self.K_proj(keys.cuda()).cuda()  # (N, T_q, C)
-        V = self.V_proj(values.cuda()).cuda()  # (N, T_q, C)
+        Q = self.Q_proj(queries)  # (N, T_q, C)
+        K = self.K_proj(keys)  # (N, T_q, C)
+        V = self.V_proj(values)  # (N, T_q, C)
         # Split and concat
         Q_ = torch.cat(torch.chunk(Q, self.num_heads, dim=2), dim=0)  # (h*N, T_q, C/h)
         K_ = torch.cat(torch.chunk(K, self.num_heads, dim=2), dim=0)  # (h*N, T_q, C/h)
@@ -55,9 +61,13 @@ class multihead_attention(nn.Module):
         key_masks = key_masks.repeat(self.num_heads, 1)  # (h*N, T_k)
         key_masks = torch.unsqueeze(key_masks, 1).repeat(1, queries.size()[1], 1)  # (h*N, T_q, T_k)
 
-        padding = Variable(torch.ones(*outputs.size()).cuda() * (-2 ** 32 + 1))
+
+        padding = Variable(torch.ones(*outputs.size()) * (-2 ** 32 + 1))
         condition = key_masks.eq(0.).float()
-        outputs = padding.cuda() * condition.cuda() + outputs.cuda() * (1. - condition.cuda())
+        if self.gpu:
+            padding = padding.cuda()
+            condition = condition.cuda()
+        outputs = padding * condition + outputs * (1. - condition)
         # Activation
         if last_layer == False:
             outputs = F.softmax(outputs, dim=-1)  # (h*N, T_q, T_k)
@@ -65,17 +75,17 @@ class multihead_attention(nn.Module):
         query_masks = torch.sign(torch.abs(torch.sum(queries, dim=-1)))  # (N, T_q)
         query_masks = query_masks.repeat(self.num_heads, 1)  # (h*N, T_q)
         query_masks = torch.unsqueeze(query_masks, 2).repeat(1, 1, keys.size()[1])  # (h*N, T_q, T_k)
-        outputs = outputs.cuda() * query_masks.cuda()
+        outputs = outputs * query_masks
         # Dropouts
         outputs = self.output_dropout(outputs)  # (h*N, T_q, T_k)
         if last_layer == True:
             #outputs = F.softmax(outputs, dim=-2)
             return outputs
         # Weighted sum
-        outputs = torch.bmm(outputs.cuda(), V_.cuda())  # (h*N, T_q, C/h)
+        outputs = torch.bmm(outputs, V_)  # (h*N, T_q, C/h)
         # Restore shape
         outputs = torch.cat(torch.chunk(outputs, self.num_heads, dim=0), dim=2)  # (N, T_q, C)
         # Residual connection
-        outputs += queries.cuda()
+        outputs += queries
 
         return outputs
